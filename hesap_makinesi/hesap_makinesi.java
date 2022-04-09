f9b35dc2c99507586a6d138ba09cc2077874b00c
basri <basriibocek@gmail.com>
2022-04-09 13:45:36 +0300

    hata giderildi

--- hesap_makinesi/hesap_makinesi.java
+++ hesap_makinesi/hesap_makinesi.java
@@ -1,11 +1,19 @@
 public class hesap_makinesi{
-    
+
 public static int toplamaYap(int a , int b){
     return a+b;
 }
+
 public static void bolmeYap(int a, int b){
     
-    double sonuc=a/b;
-    System.out.println(sonuc);
+    try{
+
+        double sonuc = a/b;
+        System.out.println(sonuc);
+}   catch(Exception e){
+
+    System.out.println("sonsuz");
+
+}
 }
 }
