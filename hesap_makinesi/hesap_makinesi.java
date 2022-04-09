50ca99db6bada1605030fb5a8eccb641671b89ed
basri <basriibocek@gmail.com>
2022-04-09 13:37:21 +0300

    bolme eklenmis sürüm

--- hesap_makinesi/hesap_makinesi.java
+++ hesap_makinesi/hesap_makinesi.java
@@ -1,6 +1,11 @@
 public class hesap_makinesi{
-
+    
 public static int toplamaYap(int a , int b){
     return a+b;
+}
+public static void bolmeYap(int a, int b){
+    
+    double sonuc=a/b;
+    System.out.println(sonuc);
 }
 }
